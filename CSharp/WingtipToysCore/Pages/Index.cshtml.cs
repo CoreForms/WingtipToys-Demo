8af585564a37855ef8da79b251a5edaa5de55e17
Michael Ketting <michael.ketting@rubicon.eu>
2023-01-30 10:37:51 +0100

    Create blank ASP.NET Core application

---
+++ CSharp/WingtipToysCore/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WingtipToys.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
