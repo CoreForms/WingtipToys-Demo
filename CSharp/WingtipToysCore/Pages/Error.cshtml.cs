f71048bef43545cbfd8758d482ac258c6192bc32
Michael Ketting <michael.ketting@rubicon.eu>
2023-01-30 11:03:21 +0100

    Disable global usings to prevent conflict with CoreForms namespaces.

--- CSharp/WingtipToysCore/Pages/Error.cshtml.cs
+++ CSharp/WingtipToysCore/Pages/Error.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace WingtipToys.Pages
 {
